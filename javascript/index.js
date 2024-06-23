80c067fc0a2e0e1092781f19ee646b5c9a4bb2e8
jeeseula <jeeseula@gmail.com>
2024-06-23 17:02:42 +0900

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+$(function () {
+  //   if ($("#nav li").first().children().hasClass("active")) {
+  //     $("#nav").removeClass("fixed-top");
+  //   } else {
+  //     $("#nav").addClass("fixed-top");
+  //   }
+});
